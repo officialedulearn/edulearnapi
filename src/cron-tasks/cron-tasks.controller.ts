c1b3250dc4872bce4e5c1d11211c93afa171f8e9
David Baiye <ozerlihashem@gmail.com>
2025-08-23 00:53:18 +0100

    Fix cron job

--- src/cron-tasks/cron-tasks.controller.ts
+++ src/cron-tasks/cron-tasks.controller.ts
@@ -3,5 +3,4 @@ import { CronTasksService } from './cron-tasks.service';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('cron-tasks')
-@UseGuards(JwtAuthGuard)
 export class CronTasksController {}
