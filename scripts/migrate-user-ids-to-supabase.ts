8dc177955d5d50e0504d6fa8fc2754dfb8dbf93e
dovidmoishe <ozerlihashem@gmail.com>
2026-05-13 03:18:30 +0100

    refactor: cut down on technical debt by migrating user ids to match supabase user ids

--- scripts/migrate-user-ids-to-supabase.ts
+++ scripts/migrate-user-ids-to-supabase.ts
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv';
 import * as path from 'path';
-import postgres from 'postgres';
+import * as postgres from 'postgres';
 
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
